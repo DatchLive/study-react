917fa4c2717c4b3896134d0e31eb0507071dbbc4
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-06-06 23:09:10 +0900

    ✨ カウンターを設置してuseStateでカウントアップする。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,15 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -28,9 +27,8 @@ export default function Home() {
       </Head>
 
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
 
       <Main page="index" />
 
