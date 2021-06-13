506febfd5b601888c4ad8c7a9276cb717d8a2638
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-06-13 18:12:08 +0900

    🎨 カスタムフックを使った

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,7 +2,9 @@ import classes from "src/components/Main/Main.module.css";
 import { Links } from "src/components/Links";
 import { Headline } from "src/components/Headline";
 
+
 export function Main(props) {
+
   return (
     <main className={classes.main}>
       <Headline page={props.page}>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,50 +1,16 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { Footer } from "../components/Footer";
-import { Main } from "../components/Main";
-import { Header } from "../components/Header";
-import { useCallback, useEffect, useState } from "react";
+import styles from "src/styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内にして下さい");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在します。");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={styles.container}>
@@ -53,9 +19,11 @@ export default function Home() {
       </Head>
 
       <Header />
+
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}> 追加</button>
       <ul>
