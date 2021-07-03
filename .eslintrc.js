95eb987d8cb87c3ec44c7b380f673e7917011b62
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-07-03 20:12:10 +0900

    ⬆️ Next.jsのアップグレードとESLintの導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node":true
-    },
-    "extends": ["plugin:react/recommended"],
-    "parserOptions": {
-        "sourceType": "module"
-    },　
-    "plugins":[],
-    "rules": {
-        "semi":"error",
-        "quotes":"error",
-        "react/prop-types":"off",
-        "no-undef": "error",
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "next"],
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: [],
+  rules: {
+    semi: "error",
+    quotes: "error",
+    "react/prop-types": "off",
+    "no-undef": "error",
+  },
 };
